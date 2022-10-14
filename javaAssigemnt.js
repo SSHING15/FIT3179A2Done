4b5268d132914a41cf52ec5b3eb9a697a8520db8
SSHING15 <samshing26@gmail.com>
2022-10-14 12:37:09 +1100

    done

---
+++ javaAssigemnt.js
@@ -0,0 +1,24 @@
+var vg_1 = "MAP.vl.json"; 
+vegaEmbed("#map_chart", vg_1).then(function(result) 
+{ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);    
+
+var vg_2 = "Linechart.vl.json"; 
+vegaEmbed("#line_chart", vg_2).then(function(result) 
+{ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);    
+
+var vg_3 = "Bubblechart.vl.json"; 
+vegaEmbed("#bubble_chart", vg_3).then(function(result) 
+{ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);    
+
+var vg_4 = "AREACHART.vl.json"; 
+vegaEmbed("#area_chart", vg_4).then(function(result) 
+{ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);   
+
+var vg_5 = "Barhcart.vl.json"; 
+vegaEmbed("#bar_chart", vg_5).then(function(result) 
+{ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);   
