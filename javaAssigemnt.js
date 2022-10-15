e9d2b9fe00304f2e9257ab47718bce1de1979e65
SSHING15 <samshing26@gmail.com>
2022-10-15 12:21:35 +1100

    ye

--- javaAssigemnt.js
+++ javaAssigemnt.js
@@ -1,24 +1,24 @@
 var vg_1 = "MAP.vl.json"; 
-vegaEmbed("#map_chart", vg_1).then(function(result) 
+vegaEmbed("#map_chart", vg_1,{"actions": false}).then(function(result) 
 { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);    
 
 var vg_2 = "Linechart.vl.json"; 
-vegaEmbed("#line_chart", vg_2).then(function(result) 
+vegaEmbed("#line_chart", vg_2,{"actions": false}).then(function(result) 
 { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);    
 
 var vg_3 = "Bubblechart.vl.json"; 
-vegaEmbed("#bubble_chart", vg_3).then(function(result) 
+vegaEmbed("#bubble_chart", vg_3,{"actions": false}).then(function(result) 
 { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);    
 
 var vg_4 = "AREACHART.vl.json"; 
-vegaEmbed("#area_chart", vg_4).then(function(result) 
+vegaEmbed("#area_chart", vg_4,{"actions": false}).then(function(result) 
 { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);   
 
 var vg_5 = "Barhcart.vl.json"; 
-vegaEmbed("#bar_chart", vg_5).then(function(result) 
+vegaEmbed("#bar_chart", vg_5,{"actions": false}).then(function(result) 
 { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);   
